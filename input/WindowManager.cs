c1f31c73e1faf8e4515185e7d439e387a9f72bad
samisadeveloper <sammmich42@gmail.com>
2026-09-08 10:46:22 -0600

    Add mouse hooks to Window Manager

--- input/WindowManager.cs
+++ input/WindowManager.cs
@@ -1,1 +1,31 @@
-// this will manage all window connections
+using System.Text.Json;
+using static WebVirtualDisplayClient.input.MouseHandler;
+
+namespace WebVirtualDisplayClient.input
+{
+        class WindowManager
+        {
+                private static SIPSorcery.Net.RTCDataChannel? mouseUpdate;
+                private static SIPSorcery.Net.RTCDataChannel? windowUpdate;
+
+                public static async void initialize() {
+                        mouseUpdate = await WebRTCClient.createDataChannel();
+                        windowUpdate = await WebRTCClient.createDataChannel();
+                }
+
+                public static void sendWindowMovement() {
+                        var data = new {channel = "window"}; // include the width, height and position of the window AND make sure to include the HWND so it can be updated
+                }
+
+                public static void updateWindowPixelData(IntPtr windowHWND) {
+                        // send the pixel data of a specific window
+                }
+
+                public static void sendMouseMovement(Point point) {
+                        var data = new {channel = "mouse", x = point.X, y = point.Y};
+
+                        mouseUpdate?.send(JsonSerializer.Serialize(data));
+                }
+
+        }
+}
