e21ab880860e2df5b582a995dec947b42afaa718
samisadeveloper <sammmich42@gmail.com>
2026-09-08 10:47:01 -0600

    Add support for mouse rendering in desktop view

--- client/src/DesktopView.tsx
+++ client/src/DesktopView.tsx
@@ -1,18 +1,51 @@
-import { useState } from "react";
+import { useState, type CSSProperties, type ReactNode } from "react";
 import { useWebRTCConnection } from "./hooks/UseWebRTCConnection";
 
-export default function DesktopView() {
-        const [latestMessage, setLatestMessage] = useState<string>('');
+import { FaMousePointer } from "react-icons/fa";
 
-        // Handle incoming stream updates directly
+type Component = {
+        channel: string;
+        x: number;
+        y: number;
+        width?: number;
+        height?: number;
+};
+
+function renderComponent(component: Component) {
+        const style = {position: 'absolute', top: component.y, left: component.x} as CSSProperties;
+
+        // TODO: replace with a lookup instead
+        if (component.channel === 'mouse') {
+                return (
+                        <FaMousePointer style={style} />
+                );
+        }
+}
+
+export default function DesktopView() {
+        const [components, setComponents] = useState<ReactNode>();
+
         const { status } = useWebRTCConnection((data) => {
-                setLatestMessage(data); 
+                const componentData = JSON.parse(data) as Component;
+
+                if (componentData) {
+                        setComponents(renderComponent(componentData));
+                }
+
         });
+
+        if (status != 'Connected') {
+                return (
+                        <div>
+                                <h1>Still connecting...</h1>
+                                <p>{status}</p>
+                        </div>
+                );
+        }
 
         return (
                 <div>
-                <p>Status: {status}</p>
-                <p>Latest Data: {latestMessage}</p>
+                        {components}
                 </div>
         );
 }
