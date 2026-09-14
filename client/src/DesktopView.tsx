b8e0688069d0f1084c73e8777d29a848d6d7c2bc
samisadeveloper <sammmich42@gmail.com>
2026-09-14 10:55:17 -0600

    feat: Support for multiple components at a time

--- client/src/DesktopView.tsx
+++ client/src/DesktopView.tsx
@@ -5,31 +5,47 @@ import { FaMousePointer } from "react-icons/fa";
 
 type Component = {
         channel: string;
+        id: number;
         x: number;
         y: number;
         width?: number;
         height?: number;
 };
 
-function renderComponent(component: Component) {
-        const style = {position: 'absolute', top: component.y, left: component.x} as CSSProperties;
+function renderComponent(component: Component) : ReactNode {
+        let style = {position: 'absolute', top: component.y, left: component.x} as CSSProperties;
 
-        // TODO: replace with a lookup instead
-        if (component.channel === 'mouse') {
-                return (
-                        <FaMousePointer style={style} />
-                );
-        }
+        // append the width and height if applicable
+        if (component.width) style = {width: component.width, ...style};
+        if (component.height) style = {height: component.height, ...style};
+
+        const renderers: Record<string, () => ReactNode> = {
+                mouse: () => <FaMousePointer style={style} />,
+                window: () => <div style={{backgroundColor: 'white', ...style}}>This is a window {component.x}, {component.y}</div>
+        };
+
+        const rendered = renderers[component.channel]?.();
+
+        return rendered;
 }
 
 export default function DesktopView() {
-        const [components, setComponents] = useState<ReactNode>();
+        const [components, setComponents] = useState<Record<string, ReactNode>>({});
 
         const { status } = useWebRTCConnection((data) => {
                 const componentData = JSON.parse(data) as Component;
 
                 if (componentData) {
-                        setComponents(renderComponent(componentData));
+                        const renderered = renderComponent(componentData);
+
+                        console.log("rendered a component", componentData);
+
+                        setComponents(prevComponents => {
+                                return {
+                                        ...prevComponents,
+                                        [componentData.id]: renderered
+                                }
+                        });
                 }
 
         });
@@ -45,7 +61,9 @@ export default function DesktopView() {
 
         return (
                 <div>
-                        {components}
+                        {Object.entries(components).map(([id, component]) => (
+                                <div key={id}>{component}</div>
+                        ))}
                 </div>
         );
 }
